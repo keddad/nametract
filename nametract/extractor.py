6d65f274a39c4e6a034e2332a6e26549aa90234d
keddad <keddad@yandex.ru>
2021-09-21 15:36:53 +0300

    Support names with -

--- nametract/extractor.py
+++ nametract/extractor.py
@@ -6,6 +6,20 @@ def maybe_name(inp: Token, minimal_name_size: int) -> bool:
     return inp.token_type == TokenEnum.Literal and len(inp.contents) >= minimal_name_size and inp.contents[0].isupper()
 
 
+def build_name(parts: List[str]) -> str:
+    tmp_s = []
+
+    for part in parts:
+        if part == "-":
+            tmp_s.append(part)
+        else:
+            if len(tmp_s) != 0 and tmp_s[-1][-1] != "-":  # Don't want to add whitespaces near "-"
+                tmp_s.append(" ")
+            tmp_s.append(part)
+
+    return "".join(tmp_s)
+
+
 def extract(inp: str, minimal_name_size=1, ignore_sentence_start=True) -> List[str]:
     """
     Extract anything that might be a name from text. Ignores things in the start of the sentence.
@@ -22,11 +36,17 @@ def extract(inp: str, minimal_name_size=1, ignore_sentence_start=True) -> List[s
     current_name = []
     ignore_next = ignore_sentence_start
 
-    for token in tokens:
+    for i, token in enumerate(tokens):
+        if token.token_type == TokenEnum.Punctuation and token.contents in "-—" and (i != len(tokens) - 1 and i != 0) \
+                and maybe_name(tokens[i - 1], minimal_name_size=minimal_name_size) \
+                and maybe_name(tokens[i + 1], minimal_name_size=minimal_name_size):
+            current_name.append("-")
+            continue
+
         if maybe_name(token, minimal_name_size) and not ignore_next:
             current_name.append(token.contents)
         elif not maybe_name(token, minimal_name_size) and len(current_name) > 0:
-            answ.append(" ".join(current_name))
+            answ.append(build_name(current_name))
             current_name = []
 
         if ignore_next:
